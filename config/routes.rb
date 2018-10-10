4aa2ad94a9d8525398996ac1bc1e28da4c5df1f6
Odell <odell.purifoy@gmail.com>
2018-10-10 13:51:21 -0400

    made a number of app wide updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   resources :tasks
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
